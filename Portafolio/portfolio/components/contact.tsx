e6ef9c7236ffd2bf9d05446a569c6db6f1f40223
mvaquerochueca <mario.vaquero.chueca@gmail.com>
2023-10-01 20:11:29 +0200

    End the portfolio

--- Portafolio/portfolio/components/contact.tsx
+++ Portafolio/portfolio/components/contact.tsx
@@ -1,50 +1,70 @@
 'use client';
 import React from 'react';
-import { useSectionInView } from '@/lib/hooks';
 import SectionHeading from './section-heading';
-import { FaPaperPlane } from 'react-icons/fa';
 import { motion } from 'framer-motion';
+import { useSectionInView } from '@/lib/hooks';
+import { sendEmail } from '@/actions/sendEmail';
+import SubmitBtn from './submit-btn';
+import toast from 'react-hot-toast';
 
 export default function Contact() {
   const { ref } = useSectionInView('Contact');
+
   return (
     <motion.section
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-      viewport={{ once: true }}
       id='contact'
       ref={ref}
-      className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center'>
+      className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center'
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}>
       <SectionHeading>Contact me</SectionHeading>
-      <p className='text-gray-700 -mt-6'>
-        Pleas contact me directly at{' '}
-        <a className='underline' href='mailto:mario.vaquero.chueca@gmail.com'>
-          mario.vaquero.chueca@gmail.com
+
+      <p className='text-gray-700 -mt-6 dark:text-white/80'>
+        Please contact me directly at{' '}
+        <a className='underline' href='mailto:example@gmail.com'>
+          example@gmail.com
         </a>{' '}
-        or trough this form.
+        or through this form.
       </p>
 
-      <form action='' className='mt-10 flex flex-col '>
+      <form
+        className='mt-10 flex flex-col dark:text-black'
+        action={async (formData) => {
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success('Email sent successfully!');
+        }}>
         <input
+          className='h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none'
+          name='senderEmail'
           type='email'
           required
           maxLength={500}
-          className='h-14 px-4 rounded-lg borderBlack'
-          placeholder='Your Email'
+          placeholder='Your email'
         />
         <textarea
-          className='h-52 my-3 rounded-lg borderBlack p-4'
-          placeholder='Your Message'
+          className='h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none'
+          name='message'
+          placeholder='Your message'
           required
-          maxLength={500}
+          maxLength={5000}
         />
-        <button
-          type='submit'
-          className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-800 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950'>
-          Submit
-          <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 ' />{' '}
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   );
